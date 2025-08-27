a30bf155c02889cde6bbdd7ce64d20819a86e53b
MykolaMS9 <mykolanikonov99@gmail.com>
2025-08-27 16:18:19 +0300

    update

--- hello_install.sh
+++ hello_install.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-
 hello_install () {
-    set -euo pipefail
-
-    # "Інсталяція" програми hello
-    INSTALL_DIR="/usr/local/bin"
-    TARGET="$INSTALL_DIR/hello"
-
-    echo '#!/usr/bin/env sh' > "$TARGET"
-    echo 'echo "Hello, world!"' >> "$TARGET"
-
-    chmod +x "$TARGET"
-
-    echo "Installed hello to $TARGET"
+  set -euo pipefail
+  INSTALL_DIR="/usr/local/bin"
+  TARGET="$INSTALL_DIR/hello"
+  echo '#!/usr/bin/env sh' > "$TARGET"
+  echo 'echo "Hello, world!"' >> "$TARGET"
+  chmod +x "$TARGET"
+  echo "Installed hello to $TARGET"
 }
